30fa9ee09097e5d5539dfb777a150edff96d844a
dven-dev <me@dven.dev>
2025-06-20 20:38:12 +0300

    add test for genDiff with JSON files

---
+++ __tests__/index.test.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('genDiff should return combined data', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  
+  expect(genDiff(filepath1, filepath2)).toBe(expected);
+});
+
