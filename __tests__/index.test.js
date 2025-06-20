a3f5da2e254d091aa4548c9d87cc0c6e4c81fffe
dven-dev <me@dven.dev>
2025-06-20 21:49:51 +0300

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,7 +5,10 @@ import genDiff from '../src/index.js';
 import { test, expect } from '@jest/globals';
 
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
@@ -13,6 +16,9 @@ test('genDiff should return combined data', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
+  const expected = readFixture('expected.json'); 
+
+  expect(genDiff(filepath1, filepath2)).toBe(expected.trim());
 });
+
 
