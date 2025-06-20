0cd2f5f56a9a05429bd6cf8bbb67f45e302ead83
dven-dev <me@dven.dev>
2025-06-20 12:37:06 +0300

    add support for two filepath arguments and -f/--format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,15 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    console.log('File 1:', filepath1);
+    console.log('File 2:', filepath2);
+    console.log('Format:', options.format || 'default');
+  });
 
 program.parse(process.argv);
+
