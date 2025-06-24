77323f4cd6022a6c6a3c6b15abbdf3145e0ecd41
dven-dev <me@dven.dev>
2025-06-24 16:01:54 +0300

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,14 +10,10 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('genDiff', () => {
-  test('should compare two JSON files', () => {
-    const filepath1 = getFixturePath('file1.json');
-    const filepath2 = getFixturePath('file2.json');
-    const expected = readFile('expected.txt').trim();
+  test('JSON format', () => {
+    expect(JSON.parse(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')))
+  .toEqual(JSON.parse(readFile('expected.txt')));
 
-    expect(genDiff(filepath1, filepath2)).toBe(expected);
   });
 });
-
-
 
